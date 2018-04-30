9efff9cc5bdd76a1efcad4c835d09b13e12b8d03
joycebrum <joycebrumu.u@gmail.com>
2018-04-29 22:29:12 -0300

    Calendario e Escola implementaas
    classes devidamente criadas e instanciadas na main

--- MerendaProject/src/merendaproject/Cardapio.java
+++ MerendaProject/src/merendaproject/Cardapio.java
@@ -1,7 +1,10 @@
 package merendaproject;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,44 +16,45 @@ import java.util.Calendar;
  * @author joycinha
  */
 public class Cardapio {
-    public ArrayList<Calendar> calendar = new ArrayList<>(); //mes de 0-11
-    private Calendar diaUm = Calendar.getInstance();
+    private ArrayList<Date> datas = new ArrayList<>(); //mes de 0-11
+    private Date inicial;
+    private Date fim;
+    private Calendar calendario;
     int mes;
     int ano;
+    
     Cardapio(int mes, int ano) {
+        calendario = Calendar.getInstance();
         this.mes = mes;
         this.ano = ano;
-        geraVetor();
-        diaUm.set(Calendar.YEAR, ano);
-        diaUm.set(Calendar.MONTH, mes);
-        diaUm.set(Calendar.DAY_OF_MONTH,1);
-    }
-    
-    private void criarData(Calendar t, int dia) {
-        t.set(Calendar.YEAR, ano);
-        t.set(Calendar.MONTH, mes);
-        t.set(Calendar.DAY_OF_MONTH,dia);
-    }
-    
-    private void geraVetor() {
-        int diaMaximo = diaUm.getActualMaximum(diaUm.DAY_OF_MONTH);
-        for(int i=0; i< diaMaximo; i++) {
-            Calendar t = Calendar.getInstance();
-            t.set(Calendar.YEAR, ano);
-            t.set(Calendar.MONTH, mes);
-            t.set(Calendar.DAY_OF_MONTH,i+1);
-            System.out.print("dia: " + t.DAY_OF_MONTH + " " + t.DAY_OF_WEEK + " i = " + i + "\n");
-            
-            if(t.DAY_OF_WEEK ==1 || t.DAY_OF_WEEK ==7) { //se e um fim de semana 
-                continue;
-            }
-            else {
-                
-                calendar.add(t);
-            }
-        }
-        System.out.print(calendar.size());
         
+        this.inicial = new Date(ano-1900, mes, 1);
+        calendario.setTime(this.inicial);
+        //dt.setDate(calendario.getActualMaximum(this.mes));
+        this.fim = new Date(ano-1900, mes,calendario.getActualMaximum(calendario.DAY_OF_MONTH));
+        montaArray();
+    }
+    private void montaArray() {
+        for(Date dt = this.inicial; dt.compareTo(this.fim) <= 0;) {
+            if(calendario.get(calendario.DAY_OF_WEEK) != 1 && calendario.get(calendario.DAY_OF_WEEK) != 7) {
+                this.datas.add(new Date(dt.getTime()));
+            }
+            calendario.add (Calendar.DATE, +1);
+            dt = calendario.getTime();
+        }
+    }
+    
+    public ArrayList<Date> getList() {
+        return this.datas;
+    }
+    public Date get(int pos) {
+        return this.datas.get(pos);
+    }
+    public Date remove(int pos) {
+        return this.datas.remove(pos);
+    }
+    public boolean remove(Date data) {
+        return this.datas.remove(data);
     }
     
 }
--- MerendaProject/src/merendaproject/MerendaProject.java
+++ MerendaProject/src/merendaproject/MerendaProject.java
@@ -5,7 +5,12 @@
  */
 
 package merendaproject;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 /**
  *
  * @author joycinha
@@ -15,11 +20,17 @@ public class MerendaProject {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        //recebe um mes e ano
+        DateFormat df = new SimpleDateFormat ("dd/MM/yyyy");
         Cardapio cardapio = new Cardapio(1,2018);
-        
+        Escola escola = new Escola("ESTADO DO RIO DE JANEIRO","PREFEITURA DE DUQUE DE CAXIAS",
+        "SECRETARIA MUNICIPAL DE EDUCAÇÃO", "SUBSECRETARIA DE EDUCAÇÃO EM INFRAESTRUTURA...", 
+        "DEPARTAMENTO DE INFRAESTRUTURA EDUCACIONAL", 33151750, "E.M. ANA DE SOUSA HERDY", "(21)3656-0364");
+        /*ArrayList<Date> lista = cardapio.getList();
+        for(Date data : lista) {
+            System.out.println(df.format(data));
+        }*/
     }
     
 }
