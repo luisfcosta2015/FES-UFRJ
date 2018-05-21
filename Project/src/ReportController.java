772fa47adcf79f99e4680b8e36e4d72e3d07336e
Filipe Ramalho <filipeRmlh@github.com>
2018-05-21 03:04:27 -0300

    Adição do itext
    Adição da biblioteca, e adaptação dos modelos.

--- Project/src/ReportController.java
+++ Project/src/ReportController.java
@@ -1,17 +1,35 @@
 
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class ReportController extends HttpServlet {
     protected DBHelper db = new DBHelper();
     public ReportController(){
         super();
     }
+    protected  void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
+        String baseUri = getServletContext().getRealPath("").split("out")[0];
+        baseUri = baseUri+Paths.get("res/reportsTemplates");
+
+        File htmlSource = new File(baseUri+"/Diario_de_Classe.html");
+        File pdfDest = new File(baseUri+"/output.pdf");
+        // pdfHTML specific code
+        ConverterProperties converterProperties = new ConverterProperties();
+        converterProperties.setBaseUri(baseUri);
+        HtmlConverter.convertToPdf(new FileInputStream(htmlSource), new FileOutputStream(pdfDest), converterProperties);
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         this.db.connect();
