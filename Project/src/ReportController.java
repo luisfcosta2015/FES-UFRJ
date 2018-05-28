b7267df921cc79bce11265ed365de704cef56b46
Henrique <henriquer@dcc.ufrj.br>
2018-05-28 00:09:41 -0300

    Junção dos módulos - criação do Orquestrador 1a parte

--- Project/src/ReportController.java
+++ Project/src/ReportController.java
@@ -12,12 +12,21 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ReportController extends HttpServlet {
     protected DBHelper db = new DBHelper();
+    protected RSSQL rssql;
+    protected HashMap<String,String> params;
+
     public ReportController(){
         super();
     }
+
+
+
     protected  void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
         String baseUri = getServletContext().getRealPath("").split("out")[0];
@@ -37,6 +46,49 @@ public class ReportController extends HttpServlet {
         //response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().println(request.getParameter("escola")+request.getParameter("ano")+request.getParameter("turma"));
+        try {
+            reqProc(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
 //        response.getWriter().write("");
     }
+
+    protected void reqProc(HttpServletRequest request) throws Exception {//Realiza todos os procedimentos após a request vinda da página
+        HashMap<String,String> stResults;
+
+        String[] dynamK = new String[0];
+        String[][] dynamResult = new String[0][];
+
+        procParams(request);
+        tarefaBanco(dynamK,dynamResult);
+        stResults=rssql.getStaticResults();
+
+    }
+
+    protected void procParams(HttpServletRequest request){
+        params = new HashMap<>();
+        Map<String, String[]> parameters;
+
+
+        Iterator<String> iterator;
+        parameters=request.getParameterMap();
+        iterator=parameters.keySet().iterator();
+        while (iterator.hasNext()){
+            params.put(iterator.next(),"");//prepara o HashMap params para ser utilizado no RSSQL
+        }
+
+    }
+
+    protected void tarefaBanco(String[] dynamK, String[][] dynamResult) throws Exception {
+        rssql = new RSSQL("rssql.json",params);
+
+
+        dynamK=rssql.getDynamicKeys();
+        dynamResult=rssql.getDynamicResults();
+    }
+
+
 }
