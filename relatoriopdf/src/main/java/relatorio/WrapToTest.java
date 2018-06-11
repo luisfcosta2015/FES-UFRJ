af7d43cbf869b804cfe1c87638c1c84bcb8137c5
Andre Muniz <andre.zinum@gmail.com>
2018-06-11 13:58:30 -0300

    Função Java que imprime os relatórios
    Colocando a parte do java para gerar relatórios no gitHub, com header e foter, e conectado ao banco.
    #MSNaoPegaMeusCodigos

---
+++ relatoriopdf/src/main/java/relatorio/WrapToTest.java
@@ -0,0 +1,5 @@
+package relatorio;
+
+public @interface WrapToTest {
+
+}
---
+++ relatoriopdf/src/test/java/jumpstart/TextFooterTest.java
@@ -0,0 +1,14 @@
+package jumpstart;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TextFooterTest {
+
+	@Test
+	public void teste1() {
+		assertTrue(true);
+	}
+
+}
