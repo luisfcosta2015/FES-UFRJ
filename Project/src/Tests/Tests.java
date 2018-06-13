259cd77fafcd8f2ba2ffa2eb21ae2763334fa24a
Henrique <henriquer@dcc.ufrj.br>
2018-06-12 22:03:39 -0300

    Diagramas e inicio do teste

---
+++ Project/src/Tests/Tests.java
@@ -0,0 +1,20 @@
+package Tests;
+import junit.framework.TestCase;
+import sslRel.helpers.DBHelper;
+
+import java.util.ArrayList;
+
+public class Tests extends TestCase {
+    public void testDBHelper(){
+
+        DBHelper object = new DBHelper();
+        boolean RetornoDB1 = object.connect();
+        ArrayList<ArrayList<String>> RetornoDB2 = object.query("SELECT * FROM escola",object);
+
+
+        assertEquals(false,RetornoDB1);
+        //assertEquals(false,RetornoDB1);
+
+
+    }
+}
