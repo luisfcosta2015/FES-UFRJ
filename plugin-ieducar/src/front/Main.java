904f01fa75b85473e6ca348c2f92857c0d8bc29b
lagaleno <galeno@dcc.ufrj.br>
2018-07-03 20:20:24 -0300

    Correcao do bug de interface

--- plugin-ieducar/src/front/Main.java
+++ plugin-ieducar/src/front/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Home/Home.fxml"));
-        primaryStage.setTitle("Relatório Customizável - Prefeitura de Caxias");
+        primaryStage.setTitle("Prefeitura de Caxias");
         primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
     }
