77485e4f18ed1727fc4332076517cc8f46364602
André Queiroz <andre_csq@hotmail.com>
2018-07-10 22:18:54 -0300

    Ultimas modificações no BD e Tela Saída

--- Merenda/src/Controller/LoteEstoque.java
+++ Merenda/src/Controller/LoteEstoque.java
@@ -7,13 +7,13 @@ import java.util.Map;
 
 public class LoteEstoque extends Lote{
         
-    public boolean cadastrar(){
+    public boolean cadastrar_entrada(){
         Conexao conn = new Conexao();
         String query = "SELECT * FROM lote_estoque le where le.id=" + getId()
             + " and le.id_alimento=" + getId_alimento();
         List<Map<String, Object>> lst = conn.query_select(query);
         
-        System.out.println(lst.get(0));
+        //System.out.println(lst.get(0));
         
         // se não tiver nenhum já cadastrado
         if (lst.isEmpty()){        
@@ -22,9 +22,33 @@ public class LoteEstoque extends Lote{
             return conn.query_update(query);
         // se já tiver cadastrado, apenas atualiza. soma o qtd_alimento
         } else {
-            // que deus me perdoe por ter feito essa gambiarra
+            // que Deus me perdoe por ter feito essa gambiarra
             int novo = Integer.parseInt(String.valueOf(lst.get(0).get("qtd_alimento")));
             novo += getQtd_alimento();
+            query = "UPDATE lote_estoque SET qtd_alimento=" + novo
+            + " WHERE id=" + getId() + " AND id_alimento=" + getId_alimento();   
+            return conn.query_update(query);
+        }        
+    }
+    
+     public boolean cadastrar_saida(){
+        Conexao conn = new Conexao();
+        String query = "SELECT * FROM lote_estoque le where le.id=" + getId()
+            + " and le.id_alimento=" + getId_alimento();
+        List<Map<String, Object>> lst = conn.query_select(query);
+        
+        //System.out.println(lst.get(0));
+        
+        // se não tiver nenhum já cadastrado
+        if (lst.isEmpty()){        
+            return false;
+        // se já tiver cadastrado, apenas atualiza. soma o qtd_alimento
+        } else {
+            // que Deus me perdoe por ter feito essa gambiarra
+            int novo = Integer.parseInt(String.valueOf(lst.get(0).get("qtd_alimento")));
+            novo -= getQtd_alimento();
+            // QUANDO O BAGULHO FICA NEGATIVO
+            if (novo < 0) return false;
             query = "UPDATE lote_estoque SET qtd_alimento=" + novo
             + " WHERE id=" + getId() + " AND id_alimento=" + getId_alimento();   
             return conn.query_update(query);
